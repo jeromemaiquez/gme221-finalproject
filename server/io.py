4c14d39cb3e41a5aee60c2a507696c9d61e4ccbb
jeromemaiquez <maiquez.jerome.ml@gmail.com>
2026-05-10 21:48:20 +0800

    Implemented road network I/O functions

---
+++ server/io.py
@@ -0,0 +1,20 @@
+import osmnx as ox
+import networkx as nx
+import geopandas as gpd
+from pathlib import Path
+
+def get_road_network(gdf: gpd.GeoDataFrame) -> nx.MultiDiGraph:
+    """Downloads the road network inside a given geopandas.GeoDataFrame."""
+    print(f"Downloading road network...")
+    polygon = gdf.geometry.union_all("coverage")
+
+    G = ox.graph_from_polygon(polygon, network_type="drive", simplify=True)
+    G = ox.distance.add_edge_lengths(G)
+    G = ox.add_edge_speeds(G)
+    G = ox.add_edge_travel_times(G)
+
+    return G
+
+def save_graph_geopackage(graph: nx.MultiDiGraph, filepath: str| Path) -> None:
+    """Save graph nodes and edges to disk as layers in a GeoPackage file."""
+    ox.save_graph_geopackage(graph, filepath)
