bce8b5f6499de609d1db9defdfd6be79cd16a4bc
jeromemaiquez <maiquez.jerome.ml@gmail.com>
2026-05-10 22:28:54 +0800

    Tested util functions & downloaded road network for AOI

---
+++ server/test.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import geopandas as gpd
+from pathlib import Path
+
+from utils import get_road_network, save_graph_geopackage
+
+WORK_DIR = Path().resolve()
+DATA_DIR = WORK_DIR / "data"
+OUTPUT_DIR = WORK_DIR / "output"
+
+fp_admin = DATA_DIR / "phl_admbnda_adm4_psa_namria_20231106.zip"
+fp_output = OUTPUT_DIR / "Antipolo_RoadNetwork.gpkg"
+
+gdf_admin = gpd.read_file(f"zip://{fp_admin}")
+
+barangays = ["Mayamot", "Cupang", "Mambugan"]
+city = "City of Antipolo"
+
+gdf_admin = gdf_admin[(gdf_admin["ADM4_EN"].isin(barangays)) & (gdf_admin["ADM3_EN"] == city)]
+
+g_roads = get_road_network(gdf_admin)
+save_graph_geopackage(g_roads, fp_output)
--- server/utils.py
+++ server/utils.py
@@ -8,7 +8,12 @@ def get_road_network(gdf: gpd.GeoDataFrame) -> nx.MultiDiGraph:
     print(f"Downloading road network...")
     polygon = gdf.geometry.union_all("coverage")
 
-    G = ox.graph_from_polygon(polygon, network_type="drive", simplify=True)
+    G = ox.graph_from_polygon(
+        polygon, 
+        network_type="all", 
+        simplify=True,
+        truncate_by_edge=True
+    )
     G = ox.distance.add_edge_lengths(G)
     G = ox.add_edge_speeds(G)
     G = ox.add_edge_travel_times(G)
